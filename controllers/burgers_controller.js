bf4b0d2c1b1f7837768d0812d4c0bd394b204cb3
Larry_A <larryagbana@gmail.com>
2020-05-07 11:40:34 -0400

    export router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,14 @@
 const express = require("express")
 const Burger = require("../models/burger")
+
+const router = express.Router()
+
+/* HTML Routes */
+router.get('/', async function (req, res) {
+    res.render('index', {})
+})
+
+/* API Routes */
+
+
+module.exports = router
