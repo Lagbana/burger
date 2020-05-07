18249bf5044bc2457dffcb332ee34ffd64dc09a4
Larry_A <larryagbana@gmail.com>
2020-05-07 11:19:54 -0400

    insert database seeds

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sub-Conscious", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Tunami", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Summer Thyme", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Peas and Thank You", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("To Err Is Cumin", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Top Bun", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mission A-Corn-Plished", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Elvis Parsley", 0);
