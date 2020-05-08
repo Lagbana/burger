ea6ec07a41ae19fd7ce40552aabcf329d2d64526
Larry_A <larryagbana@gmail.com>
2020-05-08 12:16:48 -0400

    client patch request handler

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,33 @@
+
+document.querySelectorAll(".change-devoured").forEach(button => {
+  button.addEventListener('click', function (event) {
+    const id = this.getAttribute('data-id')
+    const newburger = this.getAttribute('data-newdevour')
+    const name = this.getAttribute('name')
+
+    fetch(`/api/burgers/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({burger_name: name, devoured: newburger})
+    }).then(response => {
+      if (response.ok) location.reload()
+    })
+  })
+})
+
+
+document.getElementById('create-form').addEventListener('submit', event => {
+  event.preventDefault()
+  const newBurger = {
+    burgerName: document.getElementById('burgername').value.trim()
+  }  
+
+  fetch(`/api/burgers`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newBurger)
+  }).then(response => {
+      if (response.ok) location.reload()
+  })
+
+})
