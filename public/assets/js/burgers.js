48f2d4e0ef37545bd90fddcb2309ea476fb49c17
Larry_A <larryagbana@gmail.com>
2020-05-12 18:25:47 -0400

    fetch operations

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,33 +1,40 @@
-
-document.querySelectorAll(".change-devoured").forEach(button => {
-  button.addEventListener('click', function (event) {
-    const id = this.getAttribute('data-id')
-    const newburger = this.getAttribute('data-newdevour')
-    const name = this.getAttribute('name')
+/*
+    *Client side fetch 
+        - patch method
+*/
+document.querySelectorAll(".change-devoured").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    // update burger name and devoured using id parameter
+    const id = this.getAttribute("data-id");
+    const newburger = this.getAttribute("data-newdevour");
+    const name = this.getAttribute("name");
 
     fetch(`/api/burgers/${id}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({burger_name: name, devoured: newburger})
-    }).then(response => {
-      if (response.ok) location.reload()
-    })
-  })
-})
-
-
-document.getElementById('create-form').addEventListener('submit', event => {
-  event.preventDefault()
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ burger_name: name, devoured: newburger }),
+    }).then((response) => {
+      if (response.ok) location.reload();
+    });
+  });
+});
+/*
+      *Client side fetch 
+        - post method
+*/
+document.getElementById("create-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  // get text area input data
   const newBurger = {
-    burgerName: document.getElementById('burger-name-input').value.trim()
-  }  
+    burgerName: document.getElementById("burger-name-input").value.trim(),
+  };
 
+  // post data (newBurger) to the server
   fetch(`/api/burgers`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(newBurger)
-  }).then(response => {
-      if (response.ok) location.reload()
-  })
-
-})
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newBurger),
+  }).then((response) => {
+    if (response.ok) location.reload();
+  });
+});
