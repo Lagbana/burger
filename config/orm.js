5f066fe9d9df931e2636ccea5d38ff243cc10644
Larry_A <larryagbana@gmail.com>
2020-05-12 16:09:04 -0400

    ORM template to perform database operations

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,48 @@
-const connection = require("./connection")
+// Import dependencies
+const connection = require("./connection");
 
 const orm = {
-    selectAll: async function (tableName, orderColumn) {
-        const sql = 'SELECT * FROM ?? ORDER BY ?? ASC'
-        const [rows] = await connection.query(sql, [tableName, orderColumn])
-        return rows
-    },
-   
-    insertOne: async function (tableName, colOne, colTwo, valOfColOne, valOfColTwo) {
-        const sql = `INSERT INTO ?? (??, ??) VALUES (?, ?)`
-        const [result] = await connection.query(sql, [
-            tableName,
-            colOne,
-            colTwo,
-            valOfColOne,
-            valOfColTwo
-        ])
-        return result
-    },
-    
-    updateOne: async function (tableName, colToUpdate, searchCol, searchVal) {
-        const sql = `UPDATE ?? SET ? WHERE ?? = ?;`
-        const [result] = await connection.query(sql, [
-            tableName, 
-            colToUpdate, 
-            searchCol, 
-            searchVal
-        ])
-        return result
-    }
-}
+  /*
+   *Function selectAll: selects all from a table and order by a column
+   */
+  selectAll: async function (tableName, orderColumn) {
+    const sql = "SELECT * FROM ?? ORDER BY ?? ASC";
+    const [rows] = await connection.query(sql, [tableName, orderColumn]);
+    return rows;
+  },
+  /*
+   *Function insertOne: add a new entry into the database
+   */
+  insertOne: async function (
+    tableName,
+    colOne,
+    colTwo,
+    valOfColOne,
+    valOfColTwo
+  ) {
+    const sql = `INSERT INTO ?? (??, ??) VALUES (?, ?)`;
+    const [result] = await connection.query(sql, [
+      tableName,
+      colOne,
+      colTwo,
+      valOfColOne,
+      valOfColTwo,
+    ]);
+    return result;
+  },
+  /*
+        *Function updateOne: update an existing entry in the database
+  */
+  updateOne: async function (tableName, colToUpdate, searchCol, searchVal) {
+    const sql = `UPDATE ?? SET ? WHERE ?? = ?;`;
+    const [result] = await connection.query(sql, [
+      tableName,
+      colToUpdate,
+      searchCol,
+      searchVal,
+    ]);
+    return result;
+  },
+};
 
-module.exports = orm
+module.exports = orm;
