58a3d074d132fd1c0b81b5989be56828b2347737
Larry_A <larryagbana@gmail.com>
2020-05-12 14:17:14 -0400

    heroku mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
-const mysql = require("mysql2")
-let connection
+// Import dependencies
+const mysql = require("mysql2");
 
+let connection;
+
+// connection config for MYSQL database hosted on heroku
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL).promise()
+  connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Jameslives@2091',
-        database: 'burgers_db'
-    }).promise()
+
+// connection config for local MYSQL database
+  connection = mysql
+    .createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Jameslives@2091",
+      database: "burgers_db",
+    })
+    .promise();
 }
-connection.connect()
-module.exports = connection
+connection.connect();
+module.exports = connection;
